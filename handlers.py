360b47b39903136d4da64dd7e245662ef72639a3
Allen Schmerler <rockdhor@gmail.com>
2021-03-24 23:36:32 -0400

    fixed certain pokemon not showing properly

--- handlers.py
+++ handlers.py
@@ -1,23 +1,34 @@
 import random
 import requests
 import json
+from bs4 import BeautifulSoup
+
+commands = """```diff
++ Welcome to Wayfinder
+Thank you for your interest, here's a list of commands to get you started.
+
++pokemon show <name>
+This command shows a picture of the requested Pokemon.
+-name = {pokemon name}
+
++misu
+This command shows a random picture of a cat.
+
++help
+This command displays this message.
+```"""
 
 def get_pokemon(id):
-  id = str(id).lower()
-  url = "https://pokeapi.co/api/v2/pokemon/"+id+"/"
+  name = id.capitalize()
+  url = "https://www.pokemon.com/us/pokedex/" + name
   response = requests.get(url)
-  if str(response.status_code) == "404":
+  results = BeautifulSoup(response.text, 'html.parser')
+  try:
+    sprite = results.find("div", class_="profile-images").img['src']
+  except:
     return None
-  json_date = json.loads(response.content)
-  
-  
-
-  name = json_date['name']
-  name = name.capitalize()
-  url = random.choice(json_date['forms'])["url"]
-  response = requests.get(url)
-  json_date = json.loads(response.content)
-  sprite = json_date["sprites"]["front_default"]
+    
+  print(sprite)
   
   return (name, sprite)
 
@@ -26,3 +37,6 @@ def get_misu():
   response = requests.get(url)
   json_date = json.loads(response.content)
   return json_date[0]['url']
+
+def get_commands():
+  return (commands)
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import handlers
 from keep_alive import keep_alive
 
 client = discord.Client()
+listener = "$"
 
 @client.event
 async def on_ready():
@@ -21,19 +22,27 @@ async def on_message(message):
 
   msg = message.content
 
-  if msg.startswith('「enseñame a'):
+  if msg.startswith(listener+'pokemon show'):
     name = msg.split()[2]
     pkmn = handlers.get_pokemon(name)
     if pkmn == None:
-      await message.channel.send("Hay un tiempo y lugar para todo, pero no para eso.")
+      await message.reply("Oak's words echoed... There's a time and place for everything, but not now.")
     else:
-      await message.channel.send("Dale, aqui esta " + pkmn[0] + ".")
-      await message.channel.send(pkmn[1])
-
-  if msg.startswith('「misu'):
-    await message.channel.send(handlers.get_misu())
-  
-
+      embed = discord.Embed(title=pkmn[0]) #https://pokeapi.co/api/v2/version/1/
+      embed.set_image(url=pkmn[1])
+      response = await message.reply(embed = embed)
+      #response = await message.reply("*"+pkmn[0]+"*")
+      #pic = (await message.channel.send(""+pkmn[1]).id)
+      await response.add_reaction("⬅️")
+      await response.add_reaction("➡️")
+    return
+  if msg.startswith(listener+'misu'):
+    await message.reply(handlers.get_misu())
+    return
+  if msg.startswith(listener+'help'):
+    await message.reply("All right! Check your private messages.")
+    await message.author.send(handlers.get_commands())
+    return
  
   
     
